6e1109e05e53819211200f0422dbc3346bb2ce70
Adnan Latif <alatif.bwp@gmail.com>
2020-10-19 23:58:02 +0500

    Bind "this" with function for submit form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+  }
   render() {
     return(
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter a Store!</h2>
-          <input type="text" placeholder="Enter Store Name" defaultValue={ getFunName() }></input>
+          <input 
+          type="text" 
+          ref={ this.myInput }
+          placeholder="Enter Store Name" 
+          defaultValue={ getFunName() } />
           <button type="submit">Visit Store</button>
         </form>
     ) 
