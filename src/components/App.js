f7483693dffd5d6bad902dfa31d2b7da254abff7
Adnan Latif <alatif.bwp@gmail.com>
2020-10-20 02:52:27 +0500

    Added Sample fishes
    - Get sample data from file
    - Used it as component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import samplefishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -16,6 +17,11 @@ class App extends React.Component {
       fishes: fishes
     });
   };
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: samplefishes
+    });
+  }
   render() {
     return(
       <div className="catch-of-the-day">
@@ -23,7 +29,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div> 
     )
   }
